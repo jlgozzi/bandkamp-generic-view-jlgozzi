b99695e5227b4cda764ee74d475e9b08fc24ad05
joaolucasgozzi <lukasgozzi@gmail.com>
2022-12-19 22:50:29 -0400

    added generic views for Songs and Albuns

--- albums/views.py
+++ albums/views.py
@@ -1,33 +1,40 @@
-from rest_framework.views import APIView, status, Response
 from .models import Album
 from .serializers import AlbumSerializer
-from django.shortcuts import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from rest_framework.generics import ListCreateAPIView
 
-class AlbumView(APIView, PageNumberPagination):
+class AlbumView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request):
-        """
-        Obtençao de albums
-        """
-        albums = Album.objects.all()
+    page_size = 2
 
-        result_page = self.paginate_queryset(albums, request)
-        serializer = AlbumSerializer(result_page, many=True)
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
 
-        return self.get_paginated_response(serializer.data)
+    def perform_create(self, serializer):
+        return serializer.save(user_id=self.request.user.id)
 
-    def post(self, request):
-        """
-        Criaçao de album
-        """
-        serializer = AlbumSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
+    # def get(self, request):
+    #     """
+    #     Obtençao de albums
+    #     """
+    #     albums = Album.objects.all()
 
-        return Response(serializer.data, status.HTTP_201_CREATED)
+    #     result_page = self.paginate_queryset(albums, request)
+    #     serializer = AlbumSerializer(result_page, many=True)
+
+    #     return self.get_paginated_response(serializer.data)
+
+    # def post(self, request):
+    #     """
+    #     Criaçao de album
+    #     """
+    #     serializer = AlbumSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(user=request.user)
+
+    #     return Response(serializer.data, status.HTTP_201_CREATED)
--- songs/views.py
+++ songs/views.py
@@ -1,36 +1,29 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Song
 from rest_framework.pagination import PageNumberPagination
 from .serializers import SongSerializer
 from albums.models import Album
+from rest_framework.generics import ListCreateAPIView
 
 
-class SongView(APIView, PageNumberPagination):
+class SongView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request, pk):
-        """
-        Obtençao de musicas
-        """
-        songs = Song.objects.filter(album_id=pk)
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 
-        result_page = self.paginate_queryset(songs, request)
-        serializer = SongSerializer(result_page, many=True)
+    page_size = 2
+    
+    lookup_url_kwarg = "pk"
 
-        return self.get_paginated_response(serializer.data)
+    def get_queryset(self):
+        pk = self.kwargs[self.lookup_url_kwarg]
+        return Song.objects.filter(album_id=pk)
 
-    def post(self, request, pk):
-        """
-        Criaçao de musica
-        """
-        album = get_object_or_404(Album, pk=pk)
-
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
+        album = get_object_or_404(Album, id=self.kwargs[self.lookup_url_kwarg])
         serializer.save(album=album)
 
-        return Response(serializer.data, status.HTTP_201_CREATED)
